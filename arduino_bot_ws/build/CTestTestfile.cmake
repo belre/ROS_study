edf415fe611576ec843ed6d1231eebadc319322a
neopossam(ubuntu) <neopossam@gmail.com>
2022-10-08 17:24:11 +0900

    Digital Twin

--- arduino_bot_ws/build/CTestTestfile.cmake
+++ arduino_bot_ws/build/CTestTestfile.cmake
@@ -5,4 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("arduino_bot_description")
 subdirs("arduino_bot_test")
--- arduino_bot_ws/build/catkin_generated/setup_cached.sh
+++ arduino_bot_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export PATH='/opt/ros/noetic/bin:/home/okui/.pyenv/versions/3.9.14/bin:/home/okui/.pyenv/libexec:/home/okui/.pyenv/plugins/python-build/bin:/home/okui/.pyenv/versions/3.9.14/bin:/home/okui/.pyenv/libexec:/home/okui/.pyenv/plugins/python-build/bin:/home/okui/.pyenv/versions/3.9.14/bin:/home/okui/.pyenv/libexec:/home/okui/.pyenv/plugins/python-build/bin:/home/okui/.pyenv/versions/3.9.14/bin:/home/okui/.pyenv/libexec:/home/okui/.pyenv/plugins/python-build/bin:/home/okui/.pyenv/shims:/home/okui/.pyenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export CMAKE_PREFIX_PATH="/home/okui/ros_course/arduino_bot_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/okui/ros_course/arduino_bot_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/okui/.pyenv/versions/3.9.14/bin:/home/okui/.pyenv/libexec:/home/okui/.pyenv/plugins/python-build/bin:/home/okui/.pyenv/versions/3.9.14/bin:/home/okui/.pyenv/libexec:/home/okui/.pyenv/plugins/python-build/bin:/home/okui/.pyenv/shims:/home/okui/.pyenv/bin:/home/okui/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/okui/ros_course/arduino_bot_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/okui/ros_course/arduino_bot_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/okui/ros_course/arduino_bot_ws/src:$ROS_PACKAGE_PATH"
