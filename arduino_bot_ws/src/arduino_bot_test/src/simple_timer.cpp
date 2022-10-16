0198f0c93ee1054e61c21def91c02d836803858d
neopossam(ubuntu) <neopossam@gmail.com>
2022-10-16 13:48:59 +0900

    ROS Service CPP Implements

---
+++ arduino_bot_ws/src/arduino_bot_test/src/simple_timer.cpp
@@ -0,0 +1,25 @@
+
+#include <ros/ros.h>
+
+void timerCallback(const ros::TimerEvent& event)
+{
+    ROS_INFO("Called timerCallback function");
+}
+
+
+
+int main(int argc, char** argv) 
+{
+    ros::init(argc, argv, "timer_node");
+    ros::NodeHandle nh;
+    ros::Duration timer_duration(1);
+    ros::Timer timer = nh.createTimer(timer_duration, timerCallback);
+
+    ros::spin();
+
+    return 0;
+}
+
+
+
+
